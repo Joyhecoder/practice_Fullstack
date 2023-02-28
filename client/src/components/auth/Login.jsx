767350de87527e38330567331cffcbfb45fd08b3
Joy He <joyhe520@gmail.com>
2023-02-27 20:46:41 -0600

    login and register page styles ready

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
 const Login = () => {
   return (
-    <div>login</div>
+    <>
+    <Form className="login-form">
+      <div className="login-container">
+
+      <h3>Login</h3>
+      <br />
+      
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+    
+      <Button variant="success" type="submit">
+       Let's go
+      </Button>
+      </div>
+    </Form>
+    </>
   )
 }
 
--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
 
 const Register = () => {
   return (
-    <div>register</div>
+    <>
+      <Form className="login-form">
+        <div className="login-container">
+
+        <h3>Register</h3>
+        <br />
+        <Row>
+          <Col>
+          <Form.Label>First Name</Form.Label>
+            <Form.Control placeholder="First name" />
+          </Col>
+          <Col>
+          <Form.Label>Last Name</Form.Label>
+            <Form.Control placeholder="Last name" />
+          </Col>
+        </Row>
+
+        <br />
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      
+        <Button variant="success" type="submit">
+          Register
+        </Button>
+        </div>
+      </Form>
+    </>
   )
 }
 
